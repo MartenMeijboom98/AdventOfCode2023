8aa7a6d7652bb4937f8bfd8df786db4922fd3e62
Marten Meijboom <marten1998@hotmail.com>
2023-12-07 13:48:30 +0100

    2023 day 7

---
+++ app/src/test/kotlin/aoc/y2023/d7/p2/Part2Solver.kt
@@ -0,0 +1,199 @@
+package aoc.y2023.d7.p2
+
+import aoc.BaseSolver
+import org.junit.jupiter.api.Test
+
+val cardValues = mapOf(
+    'J' to 1,
+    '2' to 2,
+    '3' to 3,
+    '4' to 4,
+    '5' to 5,
+    '6' to 6,
+    '7' to 7,
+    '8' to 8,
+    '9' to 9,
+    'T' to 10,
+    'Q' to 12,
+    'K' to 13,
+    'A' to 14
+)
+
+class Part2Solver : BaseSolver(day = 7) {
+
+    private val hands = input.map { Hand.fromLine(it) }
+
+    @Test
+    fun `Solve part 2`() {
+        println(
+            hands.sortedWith(HandComparator()).mapIndexed { index, hand ->
+                (index + 1) * hand.bid
+            }.sum()
+        )
+    }
+
+}
+
+data class Hand(
+    val cards: List<Char>,
+    val bid: Int,
+) {
+
+    private val type = HandType.forHand(hand = this, jokerCount = cards.count { it == 'J' })
+
+    companion object {
+        fun fromLine(line: String): Hand {
+            val split = line.split(" ")
+            return Hand(
+                cards = split[0].toList(),
+                bid = split[1].toInt()
+            )
+        }
+    }
+
+    fun compareTo(other: Hand): Int {
+        var result = this.type.value.compareTo(other.type.value)
+        if (result != 0) {
+            return result
+        }
+
+        loop@ for (i in cards.indices) {
+            result = cardValues[this.cards[i]]!!.compareTo(cardValues[other.cards[i]]!!)
+            if (result != 0) {
+                break@loop
+            }
+        }
+
+        return result
+    }
+
+}
+
+class HandComparator : Comparator<Hand> {
+
+    override fun compare(o1: Hand, o2: Hand): Int =
+        o1.compareTo(o2)
+
+}
+
+sealed class HandType(val value: Int) {
+
+    companion object {
+
+        private val types = listOf(
+            FiveOfAKind(),
+            FourOfAKind(),
+            FullHouse(),
+            ThreeOfAKind(),
+            TwoPair(),
+            OnePair(),
+            HighCard()
+        )
+
+        fun forHand(hand: Hand, jokerCount: Int) =
+            if (jokerCount > 0) forHandWithJokers(hand, jokerCount)
+            else forHand(hand)
+
+        private fun forHand(hand: Hand) =
+            types.first { it.isOfType(hand) }
+
+        private fun forHandWithJokers(hand: Hand, jokerCount: Int) =
+            types.first { it.isOfType(hand, jokerCount) }
+    }
+
+    fun cardCounts(hand: Hand): Map<Char, Int> =
+        hand.cards.fold<Char, MutableMap<Char, Int>>(cardValues.keys.fold(mutableMapOf()) { acc, curr ->
+            acc[curr] = 0
+            acc
+        }) { acc, curr ->
+            acc[curr] = acc[curr]!! + 1
+            acc
+        }.let {
+            it['J'] = 0
+            it
+        }
+
+    abstract fun isOfType(hand: Hand): Boolean
+
+    abstract fun isOfType(hand: Hand, jokerCount: Int): Boolean
+}
+
+class FiveOfAKind : HandType(value = 7) {
+
+    override fun isOfType(hand: Hand): Boolean =
+        cardCounts(hand).any { it.value == 5 }
+
+    override fun isOfType(hand: Hand, jokerCount: Int): Boolean =
+        cardCounts(hand).any { it.value == (5 - jokerCount) }
+
+}
+
+class FourOfAKind : HandType(value = 6) {
+
+    override fun isOfType(hand: Hand): Boolean =
+        cardCounts(hand).any { it.value == 4 }
+
+    override fun isOfType(hand: Hand, jokerCount: Int): Boolean =
+        cardCounts(hand).any { it.value == (4 - jokerCount) }
+
+}
+
+class FullHouse : HandType(value = 5) {
+
+    override fun isOfType(hand: Hand): Boolean =
+        cardCounts(hand).let { cardCount -> cardCount.any { it.value == 3 } && cardCount.any { it.value == 2 } }
+
+    override fun isOfType(hand: Hand, jokerCount: Int): Boolean {
+        val cardCounts = cardCounts(hand)
+        val threeWithJoker = cardCounts.entries.find { it.value == (3 - jokerCount) }?.key ?: return false
+        val three = cardCounts.entries.find { it.value == 3 }?.key
+
+        return if (three != null) cardCounts.any { it.value == (2 - jokerCount) && it.key != threeWithJoker }
+        else cardCounts.any { it.value == 2 && it.key != threeWithJoker }
+    }
+
+}
+
+class ThreeOfAKind : HandType(value = 4) {
+    override fun isOfType(hand: Hand): Boolean =
+        cardCounts(hand).any { it.value == 3 }
+
+    override fun isOfType(hand: Hand, jokerCount: Int): Boolean =
+        cardCounts(hand).any { it.value == (3 - jokerCount) }
+
+}
+
+class TwoPair : HandType(value = 3) {
+
+    override fun isOfType(hand: Hand): Boolean =
+        cardCounts(hand).filter { it.value == 2 }.size == 2
+
+    override fun isOfType(hand: Hand, jokerCount: Int): Boolean {
+        val cardCounts = cardCounts(hand)
+        val firstWithJoker = cardCounts.entries.find { it.value == (2 - jokerCount) }?.key ?: return false
+        val first = cardCounts.entries.find { it.value == 2 }
+
+        return if (first != null) cardCounts.entries.any { it.value == (2 - jokerCount) && it.key != firstWithJoker }
+        else cardCounts.entries.any { it.value == 2 && it.key != firstWithJoker }
+    }
+
+}
+
+class OnePair : HandType(value = 2) {
+
+    override fun isOfType(hand: Hand): Boolean =
+        cardCounts(hand).any { it.value == 2 }
+
+    override fun isOfType(hand: Hand, jokerCount: Int): Boolean =
+        cardCounts(hand).any { it.value == (2 - jokerCount) }
+
+}
+
+class HighCard : HandType(value = 1) {
+    override fun isOfType(hand: Hand): Boolean =
+        hand.cards.distinct().size == hand.cards.size
+
+    override fun isOfType(hand: Hand, jokerCount: Int): Boolean =
+        hand.cards.distinct().size == hand.cards.size
+
+}
